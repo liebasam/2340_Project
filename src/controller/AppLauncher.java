d6e30a0ef7c12dbb0479473724b280cebe8e4258
liebasam <l.samuel.cook@gmail.com>
2016-09-20 20:39:31 -0400

    Now launches JavaFX welcome screen

--- src/controller/AppLauncher.java
+++ src/controller/AppLauncher.java
@@ -1,7 +1,22 @@
 package controller;
 
-public class AppLauncher {
-    public static void main(String... args) {
-        System.out.println("Successful launch!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppLauncher extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../view/welcome.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
