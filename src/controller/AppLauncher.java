f418aa5cd8ff06c17ef02a86915a83cdb0b8908c
Juan <juanduquevan@icloud.com>
2016-09-22 11:24:26 -0400

    Project runs with Gradle

--- src/controller/AppLauncher.java
+++ src/controller/AppLauncher.java
@@ -5,12 +5,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.net.URL;
+
+import java.net.URLClassLoader;
 
 public class AppLauncher extends Application {
     //Run -> Run 'AppLauncher'
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/welcome.fxml"));
+        System.out.println(getClass().getClassLoader().getResource("../welcome.fxml"));
+        URL[] urls = ((URLClassLoader)ClassLoader.getSystemClassLoader()).getURLs();
+        for(URL url: urls) {
+            System.out.println(url.getFile());
+        }
+        Parent root = FXMLLoader.load(getClass().getResource("../welcome.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
