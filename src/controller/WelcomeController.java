e8d7d770c720f572da7322ea4491723ef554e17e
taylor1355 <hearntaylor@gmail.com>
2016-09-27 15:38:24 -0400

    added a alert popup when a user is successfully registered removed unnecessary references to password cleaned up some code

--- src/controller/WelcomeController.java
+++ src/controller/WelcomeController.java
@@ -46,15 +46,7 @@ public class WelcomeController
     {
         String username = usernameField.getText();
         String password = passwordField.getText();
-
-        if(!isEmpty(username, password)) {
-            // TODO: This kinda demonstrates login like behavior.
-            // NEED TO ASK TA IF THIS IS OK!!!!!!
-            login(username, password);
-
-        } else {
-            createErrorMessage("Signin Error", "Not all fields are filled out");
-        }
+        login(username, password);
     }
 
     @FXML
@@ -70,6 +62,7 @@ public class WelcomeController
         } else {
             try {
                 register(username, password);
+                createMessage("Registration", "Successfully registered", "New user " + username + " created", Alert.AlertType.INFORMATION);
             } catch(IllegalArgumentException e) {
                 createErrorMessage("Registration Error", "Username already exists");
             }
@@ -95,13 +88,13 @@ public class WelcomeController
     //
 
     /**
-     * Helper method for testing if a group of strings is non-empty
-     * @param fields Strings to test
+     * Helper method for testing if a group of strings is non-null & non-empty
+     * @param fields String(s) to test
      * @return true if ANY entry is empty, false otherwise
      */
     private boolean isEmpty(String... fields) {
         for (String field : fields) {
-            if (isEmpty(field)) {
+            if (field == null || field.length() == 0) {
                 return true;
             }
         }
@@ -109,30 +102,29 @@ public class WelcomeController
     }
 
     /**
-     * Tests if a string is null or empty
-     * @param input The string to test
-     * @return true if the string is empty
-     */
-    private boolean isEmpty(String input)
-    {
-        return input == null || input.length() == 0;
-    }
-
-    /**
-     * Creates and shows an alert with the given header and message
+     * Creates and shows an error alert with the given header and message
      * @param header The error's header
      * @param message The error's message
      */
-    private void createErrorMessage(String header, String message)
-    {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
+    private void createErrorMessage(String header, String message) {
+        createMessage("Error", header, message, Alert.AlertType.ERROR);
+    }
+    
+    /**
+     * Creates and shows an alert with the given title, header, message, and alertType
+     * @param title The message's title
+     * @param header The message's header
+     * @param message The message's body text (ie message)
+     * @param alertType The type of alert the message is
+     */
+    private void createMessage(String title, String header, String message, Alert.AlertType alertType) {
+        Alert alert = new Alert(alertType);
         alert.initOwner(stage);
-        alert.setTitle("Error");
+        alert.setTitle(title);
         alert.setHeaderText(header);
         alert.setContentText(message);
         alert.show();
     }
-
 
     /**
      * Logs in the user and shows the main app
@@ -141,12 +133,12 @@ public class WelcomeController
      */
     private void login(String username, String password) {
         Model model = Model.getInstance();
-        if (model.checkAccount(username, password)) {
-            showMainApp(username, password);
-
+        if(isEmpty(username, password)) {
+            createErrorMessage("Signin Error", "Not all fields are filled out");
+        } else if (model.checkAccount(username, password)) {
+            showMainApp(username);
         } else {
-            createErrorMessage("Login Failure",
-                    "Wrong username and/or password.");
+            createErrorMessage("Signin Error", "Wrong username and/or password.");
         }
     }
 
@@ -161,11 +153,10 @@ public class WelcomeController
     }
 
     /**
-     *
-     * Calls in main page
-     *
+     * Calls in main page, user should be authenticated
+     * @param username Username of logged in user
      */
-    private void showMainApp(String username, String pw) {
+    private void showMainApp(String username) {
         try {
             // Load the fxml file and create a new stage for the popup dialog.
             FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/app.fxml"));
@@ -180,7 +171,7 @@ public class WelcomeController
 
             MainAppController mainAppCon = loader.getController();
             mainAppCon.setStage(stage);
-            mainAppCon.setUser(Model.getInstance().getAccount(username, pw));
+            mainAppCon.setUser(Model.getInstance().getAccount(username));
 
         } catch (IOException e) {
             e.printStackTrace();
